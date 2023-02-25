b93136e61b312021e99e68f8660b9b728972a455
BanesaruDenisa <77294618+BanesaruDenisa@users.noreply.github.com>
2023-02-25 10:28:00 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -22,7 +22,7 @@ public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException, InvalidDataException {
 
-        DBConnection db = new DBConnection("jdbc:mysql://localhost:3306/store", "root", "bananaverde.ro20");
+        DBConnection db = new DBConnection("jdbc:mysql://localhost:3306/store", "root", "pass");
 
         Services myServ = new Services();
 
